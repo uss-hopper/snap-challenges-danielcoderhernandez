496ac19b6f1686df9dbec2097d65bc804bce5ee2
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-12 23:04:06 -0600

    erd image added

---
+++ 10-11-19/question-two.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ERD Diagram</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<style>
+	#myDIV {width: 97.25%; height: 100px;  background: black; -webkit-animation: mymove 20s ease-in-out;
+		animation: mymove 20s ease-in-out;}
+	@-webkit-keyframes {from {background-color: darkviolet;} to {background-color: black;}}
+	@keyframes mymove {from {background-color: darkviolet;} to {background-color: black;} }
+
+	}
+</style>
+
+<body id="myDIV">
+<h2>Crows Foot</h2>
+<div class="center">
+	<img src="./erd-snapchallenge-two.jpg" class="wrap2" width="571" height="521" alt="erd">
+</div>
+
+<h2><a href="./question-one.php">userstory</a> </h2>
+</body>

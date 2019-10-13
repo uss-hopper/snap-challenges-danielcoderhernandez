4d16bca372702160bbb74783396f852ba1e1f7dd
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-12 22:48:19 -0600

    my first snap-challenge

---
+++ 10-9-19/script.js
@@ -0,0 +1,33 @@
+/*Functions
+Snap Challenge 1
+ */
+
+let x = 5500;
+let y = 200;
+
+function addNumbers (x,y) {
+let add = x + y;
+return add
+}
+console.log(addNumbers(x,y))
+
+function numberofcars(Honda, Chevy) {
+	return Honda*Chevy
+}
+
+console.log(numberofcars(3,4))
+
+
+function concatStrings(firstName, lastName) {
+	return firstName + '' + lastName;
+	//return firstName.concat(lastName);
+
+}
+
+console.log(concatStrings('Daniel','Hernandez'))
+
+
+
+
+
+

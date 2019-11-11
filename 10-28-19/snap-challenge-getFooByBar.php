088fe605b73c5731714c6689c68ee0efadcf0193
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-11 08:58:34 -0700

    behind on pushing code on other challenges

--- 10-28-19/snap-challenge-getFooByBar.php
+++ 10-28-19/snap-challenge-getFooByBar.php
@@ -32,5 +32,42 @@
 				throw (new $exceptionType($exception->getMessages(), 0, $exception));
 			}
 			$this->tweetsDate = $newTweetsDate;
-}
 
+	/**
+	 * gets the Tweet by profile id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $tweetProfileId profile id to search by
+	 * @return \SplFixedArray SplFixedArray of Tweets found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getTweetByTweetProfileId(\PDO $pdo, $tweetProfileId) : \SplFixedArray {
+
+		try {
+			$tweetProfileId = self::validateUuid($tweetProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		// create query template
+		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetProfileId = :tweetProfileId";
+		$statement = $pdo->prepare($query);
+		// bind the tweet profile id to the place holder in the template
+		$parameters = ["tweetProfileId" => $tweetProfileId->getBytes()];
+		$statement->execute($parameters);
+		// build an array of tweets
+		$tweets = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+				$tweets[$tweets->key()] = $tweet;
+				$tweets->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($tweets);
+	}
---
+++ 11-11-19/script.js
@@ -0,0 +1,19 @@
+function findRemove() {
+	//grab the original text
+	let txt = document.getElementById("find-remove").innerHTML;
+
+	//grab the words to find/replace
+	let findWord = document.getElementById("textFind").value;
+
+	//create a new regex object to use to search for findWord
+	//include the g & i modifiers: g is for global i is for case insensitive
+	//following is reg expression
+
+	let re = new RegExp(findWord, "company");
+
+	//find and remove words in the original text using the regex we created
+	let newText = txt.replace(re, findRemoveWord);
+
+	//output the new text to page
+
+	}
